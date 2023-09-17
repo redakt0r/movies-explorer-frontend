2774a4a016088f9d445ce8859ed2ed1d563f6160
Mikhail Koryakin <sdrr87@gmail.com>
2023-09-17 12:10:50 +0300

    feat: body color fix, burger btn fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -14,7 +14,7 @@ function Navigation({isLoggedIn, routeWithColoredHeader}) {
       {isLoggedIn ?
         <div className='navigation'>
           <button className='button navigation__burger-button' type='button' aria-label='Меню' onClick={openNavTab}></button>
-          <nav className='navigation__links'>
+          <nav className='navigation__links navigation__links_full'>
             <NavLink className={setActive} to='/movies' >Фильмы</NavLink>
             <NavLink className={setActive} to='/saved-movies' >Сохранённые фильмы</NavLink>
             <NavLink className={`link navigation__profile-link ${setActive} ${routeWithColoredHeader ? 'navigation__profile-link_colored' : ''}`} to='/profile'>Аккаунт</NavLink>
@@ -22,7 +22,6 @@ function Navigation({isLoggedIn, routeWithColoredHeader}) {
         </div>
          :
         <div className='navigation'>
-          <button className='button navigation__burger-button' type='button' aria-label='Меню' onClick={openNavTab}></button>
           <nav className='navigation__links'>
             <NavLink className='link navigation__signup-link' to='/singup' >Регистрация</NavLink>
             <NavLink className='link navigation__signin-link' to='/singin'>Войти</NavLink>
