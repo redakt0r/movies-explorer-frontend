921e002e26cf118a20e5644356e4688959894547
Mikhail Koryakin <sdrr87@gmail.com>
2023-09-24 21:38:23 +0300

    feat: render moviesCard with data rough

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,28 +1,58 @@
-import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import './MoviesCard.css';
-import cardImage from '../../images/card_image_sample1.jpg'
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import "./MoviesCard.css";
 
-function MoviesCard({movie}) {
+function MoviesCard({ movie }) {
   const location = useLocation();
   const [isSaved, setIsSaved] = useState(false);
   const saveMovie = () => {
     setIsSaved(true);
-  }
+  };
 
-  const routeWithSavedList = location.pathname === '/saved-movies'
+  const routeWithSavedList = location.pathname === "/saved-movies";
+
+  const movieDuration = (duration) => {
+    return `${Math.floor(duration / 60)}ч ${duration % 60}м`;
+  };
+
+  const fullCoverUrl = (url) => {
+    return "https://api.nomoreparties.co/." + url;
+  };
 
   return (
-    <li className='movies-card'>
-      <article className='movies-card__item'>
-        <div className='movies-card__info'>
-          <h2 className='movies-card__title'>В погоне за Бенкси</h2>
-          <p className='movies-card__duration'>0ч 42м</p>
+    <li className="movies-card">
+      <article className="movies-card__item">
+        <div className="movies-card__info">
+          <h2 className="movies-card__title">{movie.nameRU}</h2>
+          <p className="movies-card__duration">
+            {movieDuration(movie.duration)}
+          </p>
         </div>
-        <img className='movies-card__cover' src={cardImage} alt={`Обложка фильма '${movie.nameRU}'`} />
-        {routeWithSavedList ?
-        <button className='button movies-card__button movies-card__button_in-list' type='button' aria-label='Удалить' ></button> :
-        <button className={`button movies-card__button ${isSaved ? 'movies-card__button_saved' : ''}`} type='button' aria-label={isSaved ? 'Фильм сохранен' : 'Сохранить'} onClick={saveMovie}>{isSaved ? '' : 'Сохранить'}</button>}
+        <a href={movie.trailerLink} target="blank">
+          <img
+            className="movies-card__cover"
+            src={fullCoverUrl(movie.image.url)}
+            alt={`Обложка фильма '${movie.nameRU}'`}
+          />
+        </a>
+        {routeWithSavedList ? (
+          <button
+            className="button movies-card__button movies-card__button_in-list"
+            type="button"
+            aria-label="Удалить"
+          ></button>
+        ) : (
+          <button
+            className={`button movies-card__button ${
+              isSaved ? "movies-card__button_saved" : ""
+            }`}
+            type="button"
+            aria-label={isSaved ? "Фильм сохранен" : "Сохранить"}
+            onClick={saveMovie}
+          >
+            {isSaved ? "" : "Сохранить"}
+          </button>
+        )}
       </article>
     </li>
   );
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,27 +1,48 @@
-import { useLocation } from 'react-router-dom';
-import MoviesCard from '../MoviesCard/MoviesCard';
-import './MoviesCardList.css';
+import { useLocation } from "react-router-dom";
+import MoviesCard from "../MoviesCard/MoviesCard";
+import "./MoviesCardList.css";
+import Preloader from "../Preloader/Preloader";
+import { useState } from "react";
 
-function MoviesCardList() {
+function MoviesCardList({}) {
+  const [movies, setMovies] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
-  const routeWithMoreButton = location.pathname === '/movies'
+  const routeWithMoreButton = location.pathname === "/movies";
+  const allMovies = JSON.parse(localStorage.getItem("moviesList"));
   const moviesSample = [
-    {nameRU: 'Хороший фильм'},
-    {nameRU: 'Средний фильм'},
-    {nameRU: 'Плохой фильм'},
-    {nameRU: 'Ёлки 5'}
-  ]
-  console.log(moviesSample[0])
+    allMovies[0],
+    allMovies[1],
+    allMovies[2],
+    allMovies[3],
+    allMovies[4],
+    allMovies[5],
+  ];
+  console.log(moviesSample);
   return (
-    <section className='section movies-list' aria-label='Список фильмов'>
-      <ul className='movies-list__list'>
-        <MoviesCard movie={moviesSample[0]}/>
-        <MoviesCard movie={moviesSample[1]}/>
-        <MoviesCard movie={moviesSample[2]}/>
-        <MoviesCard movie={moviesSample[3]}/>
-      </ul>
-      {routeWithMoreButton && <button className='button movies-list__button' type='button' aria-label='Ещё фильмы'>Ещё</button>}
-    </section>
+    <>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <section className="section movies-list" aria-label="Список фильмов">
+          <ul className="movies-list__list">
+            <MoviesCard movie={moviesSample[0]} />
+            <MoviesCard movie={moviesSample[1]} />
+            <MoviesCard movie={moviesSample[2]} />
+            <MoviesCard movie={moviesSample[3]} />
+          </ul>
+          {routeWithMoreButton && (
+            <button
+              className="button movies-list__button"
+              type="button"
+              aria-label="Ещё фильмы"
+            >
+              Ещё
+            </button>
+          )}
+        </section>
+      )}
+    </>
   );
 }
 
