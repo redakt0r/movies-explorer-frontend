6ba10f5382fc2f0d2a2cdf2217314f27f4ff21d4
Mikhail Koryakin <sdrr87@gmail.com>
2023-09-25 18:47:32 +0300

    feat: searchForm params and error

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,9 +1,9 @@
 import "./FilterCheckbox.css";
 
-function FilterCheckbox() {
+function FilterCheckbox({onChange}) {
   return (
     <div className="filter-checkbox">
-      <input className="filter-checkbox__checkbox" type="checkbox" id="switch"/>
+      <input className="filter-checkbox__checkbox" type="checkbox" id="switch" onChange={onChange} name="isShort"/>
       <label className="filter-checkbox__switch" htmlFor="switch"></label>
       <p className="filter-checkbox__title">Короткометражки</p>
     </div>
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,21 +1,46 @@
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import "./SearchForm.css";
+import { useState } from "react";
 
 function SearchForm() {
+  const [searchParams, setSearchParams] = useState({movie: '', isShort: false});
+  const [searchError, setSearchError] = useState('');
+
+  const handleSearchParams = (e) => {
+    const target = e.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    setSearchParams({ ...searchParams, [name]: value });
+    setSearchError('');
+  };
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log(searchParams);
+    if (!searchParams.movie) {
+      setSearchError('Введите ключевое слово.')
+    } else setSearchError('')
+  };
+
   return (
     <section className="section search-form" aria-label="Поиск фильмов">
-      <form className="search-form__form" name="search">
+      <form className="search-form__form" name="search" onSubmit={search}>
         <input
           className="search-form__input"
           type="text"
           name="movie"
           placeholder="Фильм"
-          required
-          minLength={2}
-          maxLength={30}
+          onChange={handleSearchParams}
         />
-        <button className="button search-form__button" type="submit" aria-label="Поиск">Поиск</button>
-        <FilterCheckbox/>
+        <button
+          className="button search-form__button"
+          type="submit"
+          aria-label="Поиск"
+        >
+          Поиск
+        </button>
+        <span className="search-form__error">{searchError}</span>
+        <FilterCheckbox onChange={handleSearchParams} />
       </form>
     </section>
   );
